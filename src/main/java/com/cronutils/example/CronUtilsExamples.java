fd5f36dff4fde48720f2599efa139d690d928ed5
Jose Martin Rozanec <jmrozanec@gmail.com>
2016-07-16 17:56:48 -0700

    Add cron description example!

--- src/main/java/com/cronutils/example/CronUtilsExamples.java
+++ src/main/java/com/cronutils/example/CronUtilsExamples.java
@@ -1,6 +1,7 @@
 package com.cronutils.example;
 
 import com.cronutils.builder.CronBuilder;
+import com.cronutils.descriptor.CronDescriptor;
 import com.cronutils.mapper.CronMapper;
 import com.cronutils.model.Cron;
 import com.cronutils.model.definition.CronDefinition;
@@ -10,52 +11,13 @@ import com.cronutils.parser.CronParser;
 import org.joda.time.DateTime;
 import org.joda.time.Duration;
 
+import java.util.Locale;
+
 import static com.cronutils.model.CronType.QUARTZ;
 import static com.cronutils.model.CronType.UNIX;
 import static com.cronutils.model.field.expression.FieldExpressionFactory.*;
 
 public class CronUtilsExamples {
-
-    public static CronDefinition defineOwnCronDefinition() {
-        //define your own cron: arbitrary fields are allowed and last field can be optional
-        return CronDefinitionBuilder.defineCron()
-                .withSeconds().and()
-                .withMinutes().and()
-                .withHours().and()
-                .withDayOfMonth()
-                .supportsHash().supportsL().supportsW().and()
-                .withMonth().and()
-                .withDayOfWeek()
-                .withIntMapping(7, 0) //we support non-standard non-zero-based numbers!
-                .supportsHash().supportsL().supportsW().and()
-                .withYear().and()
-                .lastFieldOptional()
-                .instance();
-    }
-
-    public static Cron buildQuartzCronExpressionUsingCronBuilder(){
-        //Create a cron expression. CronMigrator will ensure you remain cron provider agnostic
-        return CronBuilder.cron(CronDefinitionBuilder.instanceDefinitionFor(QUARTZ))
-                .withYear(always())
-                .withDoM(between(1, 3))
-                .withMonth(always())
-                .withDoW(questionMark())
-                .withHour(always())
-                .withMinute(always())
-                .withSecond(on(0))
-                .instance();
-    }
-
-    public static Cron buildUnixCronExpressionUsingCronBuilder(){
-        //Create a cron expression. CronMigrator will ensure you remain cron provider agnostic
-        return CronBuilder.cron(CronDefinitionBuilder.instanceDefinitionFor(UNIX))
-                .withDoM(between(1, 3))
-                .withMonth(always())
-                .withDoW(always())
-                .withHour(always())
-                .withMinute(always())
-                .instance();
-    }
 
     public static void main(String[] args) {
         //Using CronBuilder we can build cron expressions for any format.
@@ -133,5 +95,58 @@ public class CronUtilsExamples {
                         parsedQuartzCronExpression.asString(), now, timeToNextExecution.toStandardSeconds().getSeconds()
                 )
         );
+
+        //To finish this demo, we would like to showcase another useful feature: human readable descriptions
+        //Cron expressions are cryptic to most non-tech users: how can we bring some transparency to them?
+        //We add the cron-utils dependency to the project, and ask for a human readable description!
+        CronDescriptor descriptor = CronDescriptor.instance(Locale.US);//we support multiple languages! Just pick one!
+        String quartzBuiltCronExpressionDescription = descriptor.describe(quartzBuiltCronExpression);
+        System.out.println(
+                String.format("Quartz expression '%s' is described as '%s'", quartzBuiltCronExpression.asString(), quartzBuiltCronExpressionDescription)
+        );
+        //That's all! Join us on Gitter and send us feedback on how are you doing with cron-utils!
+    }
+
+
+
+    private static CronDefinition defineOwnCronDefinition() {
+        //define your own cron: arbitrary fields are allowed and last field can be optional
+        return CronDefinitionBuilder.defineCron()
+                .withSeconds().and()
+                .withMinutes().and()
+                .withHours().and()
+                .withDayOfMonth()
+                .supportsHash().supportsL().supportsW().and()
+                .withMonth().and()
+                .withDayOfWeek()
+                .withIntMapping(7, 0) //we support non-standard non-zero-based numbers!
+                .supportsHash().supportsL().supportsW().and()
+                .withYear().and()
+                .lastFieldOptional()
+                .instance();
+    }
+
+    private static Cron buildQuartzCronExpressionUsingCronBuilder(){
+        //Create a cron expression. CronMigrator will ensure you remain cron provider agnostic
+        return CronBuilder.cron(CronDefinitionBuilder.instanceDefinitionFor(QUARTZ))
+                .withYear(always())
+                .withDoM(between(1, 3))
+                .withMonth(always())
+                .withDoW(questionMark())
+                .withHour(always())
+                .withMinute(always())
+                .withSecond(on(0))
+                .instance();
+    }
+
+    private static Cron buildUnixCronExpressionUsingCronBuilder(){
+        //Create a cron expression. CronMigrator will ensure you remain cron provider agnostic
+        return CronBuilder.cron(CronDefinitionBuilder.instanceDefinitionFor(UNIX))
+                .withDoM(between(1, 3))
+                .withMonth(always())
+                .withDoW(always())
+                .withHour(always())
+                .withMinute(always())
+                .instance();
     }
 }
